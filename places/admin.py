405e035579dd0df844d7e40cf33d230628c6a855
Shashi Kumar <shashiessp@gmail.com>
2016-04-23 16:38:23 +0530

    add places

---
+++ places/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from places.models import Place, Feedback
+
+admin.site.register(Place)
+admin.site.register(Feedback)
---
+++ places/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from profiles.models import UserProfile
+
+
+class Place(models.Model):
+	name = models.CharField(max_length=250)
+	latitiude = models.FloatField()
+	longitude = models.FloatField()
+	description = models.TextField()
+	thumbnail = models.URLField()
+
+	def __unicode__(self):
+		return self.name
+
+
+class Feedback(models.Model):
+	place = models.ForeignKey(Place)
+	description = models.TextField()
+	is_postive = models.BooleanField(default=None)
+	given_by = models.ForeignKey(UserProfile)
+
+	def __unicode__(self):
+		return self.place.description
